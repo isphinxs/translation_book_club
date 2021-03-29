ee4ab3b13accbfd5b53f35194108b3a4cfe85ee3
Samantha Ostrowski <isphinxs@hotmail.com>
2021-03-28 22:10:41 -0400

    Fix Publisher associations

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,4 +1,2 @@
 class PublishersController < ApplicationController
-    has_many :books
-    has_many :languages, through: :books
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+members = Member.create([{ name: "Sam" }, { name: "Jordan" }])
+authors = Author.create([{ name: "Alia Trabucco Zeran"}, { name: "Negar Djavadi" }])
+languages = Language.create([{ name: "Spanish" }, { name: "French" }])
+publishers = Publisher.create([{ name: "Coffee House Press" }, { name: "Europa Editions"}])
+books = Book.create([{ name: "The Remainder" }, { name: "Disoriental" }])
+
+members.first.books << books.first
+authors.first.books << books.first
+languages.first.books << books.first
+publishers.first.books << books.first
+
+members.last.books << books.last
+authors.last.books << books.last
+languages.last.books << books.last
+publishers.last.books << books.last
