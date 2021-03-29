a87933d62821b10cce1e70f2c9736c437dd6546f
Samantha Ostrowski <isphinxs@hotmail.com>
2021-03-29 18:29:36 -0400

    Add Language controller and views

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,2 +1,48 @@
 class LanguagesController < ApplicationController
+    def index
+        @languages = Language.all
+    end
+
+    def show
+        set_language
+    end
+
+    def new
+        @language = Language.new
+    end
+
+    def create
+        @language = Language.new(language_params)
+        if @language.save
+            redirect_to language_path(@language)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        set_language
+    end
+    
+    def update
+        set_language
+        @language.update(language_params)
+        redirect_to language_path(@language)
+    end
+
+    # def destroy
+    #     set_author
+    #     @author.destroy
+    #     redirect_to authors_path
+    # end
+
+    private
+
+    def set_language
+        @language = Language.find_by(id: params[:id])
+    end
+
+    def language_params
+        params.require(:language).permit(:name)
+    end
 end
